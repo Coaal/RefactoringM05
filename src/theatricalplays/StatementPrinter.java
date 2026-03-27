932dc942a98b4833ba0796718df4f249d2c5916e
Alfredo <39448625z@iespoblenou.org>
2026-03-27 09:03:29 +0100

    1 COMMIT AMB rename de frmt a format i perf a performance

--- src/theatricalplays/StatementPrinter.java
+++ src/theatricalplays/StatementPrinter.java
@@ -1,7 +1,5 @@
 //he fet rename de frmt y perf perque podrien no entendres.
-//
-//
-//
+
 package theatricalplays;
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -20,27 +18,22 @@ public class StatementPrinter {
             var play = plays.get(performance.playID);
             var thisAmount = 0;
 
-            private int amountFor(Performance performance, Play play) {
-                int result = 0;
-
-                switch (play.type) {
-                    case "tragedy":
-                        result = 40000;
-                        if (performance.audience > 30) {
-                            result += 1000 * (performance.audience - 30);
-                        }
-                        break;
-                    case "comedy":
-                        result = 30000;
-                        if (performance.audience > 20) {
-                            result += 10000 + 500 * (performance.audience - 20);
-                        }
-                        result += 300 * performance.audience;
-                        break;
-                    default:
-                        throw new Error("unknown type: " + play.type);
-                }
-                return result;
+            switch (play.type) {
+                case "tragedy":
+                    thisAmount = 40000;
+                    if (performance.audience > 30) {
+                        thisAmount += 1000 * (performance.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    thisAmount = 30000;
+                    if (performance.audience > 20) {
+                        thisAmount += 10000 + 500 * (performance.audience - 20);
+                    }
+                    thisAmount += 300 * performance.audience;
+                    break;
+                default:
+                    throw new Error("unknown type: ${play.type}");
             }
 
             // add volume credits
