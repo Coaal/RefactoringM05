80a25916f661c99fca1ac5fcfe2ef878eed979cb
Alfredo <39448625z@iespoblenou.org>
2026-03-27 09:16:23 +0100

    2 commit amb extract method del switch (calculateAmount)

--- src/theatricalplays/StatementPrinter.java
+++ src/theatricalplays/StatementPrinter.java
@@ -1,6 +1,5 @@
-//he fet rename de frmt y perf perque podrien no entendres.11
-
 package theatricalplays;
+
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Map;
@@ -16,38 +15,54 @@ public class StatementPrinter {
 
         for (var performance : invoice.performances) {
             var play = plays.get(performance.playID);
-            var thisAmount = 0;
 
-            switch (play.type) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (performance.audience > 30) {
-                        thisAmount += 1000 * (performance.audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (performance.audience > 20) {
-                        thisAmount += 10000 + 500 * (performance.audience - 20);
-                    }
-                    thisAmount += 300 * performance.audience;
-                    break;
-                default:
-                    throw new Error("unknown type: ${play.type}");
+            var thisAmount = calculateAmount(performance, play);
+
+            volumeCredits += Math.max(performance.audience - 30, 0);
+
+            if ("comedy".equals(play.type)) {
+                volumeCredits += performance.audience / 5;
             }
 
-            // add volume credits
-            volumeCredits += Math.max(performance.audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy".equals(play.type)) volumeCredits += Math.floor(performance.audience / 5);
+            result += String.format(
+                    "  %s: %s (%s seats)\n",
+                    play.name,
+                    format.format(thisAmount / 100),
+                    performance.audience
+            );
 
-            // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, format.format(thisAmount / 100), performance.audience);
             totalAmount += thisAmount;
         }
+
         result += String.format("Amount owed is %s\n", format.format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
+
         return result;
     }
 
+    private int calculateAmount(Performance performance, Play play) {
+        int result = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (performance.audience > 30) {
+                    result += 1000 * (performance.audience - 30);
+                }
+                break;
+
+            case "comedy":
+                result = 30000;
+                if (performance.audience > 20) {
+                    result += 10000 + 500 * (performance.audience - 20);
+                }
+                result += 300 * performance.audience;
+                break;
+
+            default:
+                throw new Error("unknown type: " + play.type);
+        }
+
+        return result;
+    }
 }
