b0342cdd5095c0272bee470ada1fc49bd7065158
Alfredo <39448625z@iespoblenou.org>
2026-03-27 09:00:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RefactoringM05
---
+++ src/theatricalplays/StatementPrinter.java
@@ -0,0 +1,60 @@
+//he fet rename de frmt y perf perque podrien no entendres.
+//
+//
+//
+package theatricalplays;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Map;
+
+public class StatementPrinter {
+
+    public String print(Invoice invoice, Map<String, Play> plays) {
+        var totalAmount = 0;
+        var volumeCredits = 0;
+        var result = String.format("Statement for %s\n", invoice.customer);
+
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+
+        for (var performance : invoice.performances) {
+            var play = plays.get(performance.playID);
+            var thisAmount = 0;
+
+            private int amountFor(Performance performance, Play play) {
+                int result = 0;
+
+                switch (play.type) {
+                    case "tragedy":
+                        result = 40000;
+                        if (performance.audience > 30) {
+                            result += 1000 * (performance.audience - 30);
+                        }
+                        break;
+                    case "comedy":
+                        result = 30000;
+                        if (performance.audience > 20) {
+                            result += 10000 + 500 * (performance.audience - 20);
+                        }
+                        result += 300 * performance.audience;
+                        break;
+                    default:
+                        throw new Error("unknown type: " + play.type);
+                }
+                return result;
+            }
+
+            // add volume credits
+            volumeCredits += Math.max(performance.audience - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy".equals(play.type)) volumeCredits += Math.floor(performance.audience / 5);
+
+            // print line for this order
+            result += String.format("  %s: %s (%s seats)\n", play.name, format.format(thisAmount / 100), performance.audience);
+            totalAmount += thisAmount;
+        }
+        result += String.format("Amount owed is %s\n", format.format(totalAmount / 100));
+        result += String.format("You earned %s credits\n", volumeCredits);
+        return result;
+    }
+
+}
